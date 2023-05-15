506f0b750a76aa23f29bca8c354aa026ccc65d55
LFF5644 <meerschweinchen5644@gamil.com>
2023-05-15 22:29:47 +0200

    first working progress! ... but long road to target!

---
+++ app.js
@@ -0,0 +1,114 @@
+/**
+	@type {typeof import('lui/index')}
+*/
+const lui=window.lui;
+const {
+	init,
+	node,
+	node_dom,
+	node_map,
+	hook_effect,
+	hook_memo,
+	hook_model,
+}=lui;
+
+const model={
+	init: ()=>({
+		view: "overview",
+		account: null,
+	}),
+	set: (state,key,value)=>({
+		...state,
+		[key]: value,
+	}),
+};
+const model_play={
+	init:()=>({
+		allowChangePlayback: false,
+		connected: false,
+		currentlyPlaying: null,
+	}),
+	set: (state,key,value)=>({
+		...state,
+		[key]: value,
+	}),
+};
+
+function getToken(){
+	const cookie=document.cookie.split("; ").find(item=>item.startsWith("token="));
+	if(cookie) return cookie.substring(6);
+	return null;
+}
+
+function ViewOverview({state,musikPlayer,spotify}){return[
+	node_dom("h1[innerText=Übersicht]"),
+	node(ViewOverviewMusikPlayer,{state,musikPlayer}),
+	node(ViewOverviewSpotify,{state,spotify}),
+
+]}
+function ViewOverviewMusikPlayer({state,musikPlayer}){return[
+	node_dom("p[innerText=coming later ...]"),
+]}
+function ViewOverviewSpotify({state,spotify}){return[
+	node_dom("textarea",{
+		innerHTML: JSON.stringify(spotify.state.currentlyPlaying,null,2).split("\n").join("&#10;"),
+		rows: JSON.stringify(spotify.state.currentlyPlaying,null,2).split("\n").length,
+		width: "100%",
+	}),
+]}
+
+init(()=>{
+	const [state,actions]=hook_model(model);
+	const musikPlayer={};
+	const spotify={};
+	{
+		const [state,actions]=hook_model(model_play);
+		musikPlayer.state=state;
+		musikPlayer.actions=actions;
+	}
+	{
+		const [state,actions]=hook_model(model_play);
+		spotify.state=state;
+		spotify.actions=actions;
+	}
+	
+	hook_effect(()=>{
+		window.actions=actions;
+		initMusikPlayer:{
+			window.musikPlayer={};
+			window.musikPlayer.actions=musikPlayer.actions;
+		}
+		initSpotify:{
+			window.spotify={};
+			window.spotify.actions=spotify.actions;
+			window.spotify.socket=io({
+				path: "/bind/socket/currently-playing/spotify",
+				auth: {
+					token: getToken(),
+				},
+			});
+			window.spotify.socket.on("init",data=>{
+				const {
+					account,
+					allowChangePlayback,
+				}=data;
+				console.log("account",account);
+				actions.set("account",account);
+				console.log("allowChangePlayback",allowChangePlayback);
+				spotify.actions.set("allowChangePlayback",allowChangePlayback);
+			});
+			window.spotify.socket.on("set-infos",currentlyPlaying=>{
+				console.log("currentlyPlaying",currentlyPlaying);
+				spotify.actions.set("currentlyPlaying",currentlyPlaying);
+			});
+			window.spotify.socket.emit("get-infos");
+		}
+
+	});
+
+	return[null,[
+
+		state.view==="overview"&&
+		node(ViewOverview,{state,musikPlayer,spotify}),
+	]];
+})
