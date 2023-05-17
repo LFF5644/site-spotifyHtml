d8b85a974e7cc8bf613d47a73b13fcc5df28a91a
LFF5644 <meerschweinchen5644@gamil.com>
2023-05-17 21:54:20 +0200

    playbackaktions

--- app.js
+++ app.js
@@ -56,6 +56,18 @@ function getToken(){
 	if(cookie) return cookie.substring(6);
 	return null;
 }
+function timeToStr(ms){
+	const numToStr=num=>String(num).padStart(2,"0");
+	let sec=Math.floor(ms/1000); // Millisekunden zu Sekunden;
+
+	let min=Math.floor(sec/60);
+	sec=sec%60;
+
+	let hou=Math.floor(min/60);
+	min=min%60;
+	const time=`${hou===0?"":numToStr(hou)+":"}${numToStr(min)}:${numToStr(sec)}`
+	return time;
+}
 
 function ViewOverview({state,musikPlayer,spotify}){return[
 	node_dom("h1[innerText=Übersicht]"),
@@ -67,6 +79,73 @@ function ViewOverviewMusikPlayer({state,musikPlayer}){return[
 	node_dom("p[innerText=coming later ...]"),
 ]}
 function ViewOverviewSpotify({state,spotify}){return[
+	spotify.state.currentlyPlaying===null&&
+	node_dom("p[innerText=Keine Wiedergabe auf Spotify][style=color:red]"),
+
+	spotify.state.currentlyPlaying!==null&&
+	node_dom("div",null,[
+		node_dom("p",null,[
+			node_dom("span[innerText=Song: ]"),
+			node_dom("a",{
+				innerText: spotify.state.currentlyPlaying.track.name,
+				href: spotify.state.currentlyPlaying.track.url,
+				onclick:()=> confirm(`Weiter auf Spotify?\n${spotify.state.currentlyPlaying.track.name}\n\n${spotify.state.currentlyPlaying.track.url}`)
+			})
+		]),
+
+		spotify.state.currentlyPlaying.track.imgs.length>2&&
+		node_dom("p",null,[
+			node_dom("img",{
+				src: spotify.state.currentlyPlaying.track.imgs.find(item=>item.height===300&&item.width===300).url,
+			}),
+		]),
+
+		node_dom("p[style=display:flex;align-items:center]",{
+			S:{
+				"align-items": "center",
+				color: spotify.state.currentlyPlaying.playing?"green":"red",
+				display: "flex",
+			},
+		},[
+			node_dom("span",{
+				innerText: timeToStr(spotify.state.currentlyPlaying.track.progress),
+			}),
+			node_dom("input[disabled=true][type=range][style=margin-right:10px;margin-left:10px]",{
+				value: spotify.state.currentlyPlaying.track.progress,
+				max: spotify.state.currentlyPlaying.track.length,
+			}),
+			node_dom("span",{
+				innerText: timeToStr(spotify.state.currentlyPlaying.track.length),
+			}),
+		]),
+
+		spotify.state.currentlyPlaying.track.mp3&&
+		node_dom("p[style=display:flex;align-items:center]",null,[
+			node_dom("span[innerText=Rein hören: ]"),
+			node_dom("audio[preload][controls][style=height:20px]",{
+				src: spotify.state.currentlyPlaying.track.mp3,
+			}),
+		]),
+
+		spotify.state.allowChangePlayback&&
+		node_dom("div",null,[
+			node_dom("p",null,[
+				node_dom("button[innerText=<]",{
+					onclick:()=>spotify.socket.emit("playbackAction",{action:"previous"}),
+				}),
+				node_dom("button[innerText=Play]",{
+					onclick:()=>spotify.socket.emit("playbackAction",{action:"play"}),
+				}),
+				node_dom("button[innerText=Pause]",{
+					onclick:()=>spotify.socket.emit("playbackAction",{action:"pause"}),
+				}),
+				node_dom("button[innerText=>]",{
+					onclick:()=>spotify.socket.emit("playbackAction",{action:"next"}),
+				}),
+			]),
+			
+		]),
+	]),
 	node_dom("textarea",{
 		innerHTML: JSON.stringify(spotify.state.currentlyPlaying,null,2).split("\n").join("&#10;"),
 		rows: JSON.stringify(spotify.state.currentlyPlaying,null,2).split("\n").length+2,
@@ -138,7 +217,6 @@ init(()=>{
 	});
 
 	return[null,[
-
 		state.view==="overview"&&
 		node(ViewOverview,{state,musikPlayer,spotify}),
 	]];
